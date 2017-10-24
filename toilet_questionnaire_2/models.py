fcac0d737a000e7dc45b23b1b09585cfdfa17491
Alek Spitzer <alekspitzer@MacBook-Pro-de-M2015.local>
2017-10-24 06:49:30 -0600

    update questionnaire 2

--- toilet_questionnaire_2/models.py
+++ toilet_questionnaire_2/models.py
@@ -10,6 +10,7 @@ from otree import widgets
 from otree.common import Currency as c, currency_range, safe_json
 from otree.constants import BaseConstants
 from otree.models import BaseSubsession, BaseGroup, BasePlayer
+
 # </standard imports>
 
 author = 'Your name here'
@@ -26,7 +27,13 @@ class Constants(BaseConstants):
 
 
 class Subsession(BaseSubsession):
-    pass
+    def before_session_starts(self):
+
+        for group in self.get_groups():
+            for player in group.get_players():
+                player.q2_best_player = models.CharField(
+                    max_length=255, widget=widgets.RadioSelect(),
+                    choices=player.get_others_in_group())
 
 
 class Group(BaseGroup):
@@ -35,39 +42,22 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
 
-    def __init__(self):
-        self.q2_best_player = models.CharField(
-            max_length=255, widget=widgets.RadioSelect(),
-            choices = self.get_choices(self.get_participants()))
-
-    def get_participants(self):
-        builder_list = []
-        for player in self.get_others_in_group():
-            builder_list.append(player.participant.id_in_session)
-
-        return builder_list
-
-    def get_choices(self, participants):
-        choices = []
-        choices.append(self)
-        for participant in participants:
-            choices.append(participant)
-
-        return choices
-
     q2_communication_surprised = models.PositiveIntegerField(min=1, max=5, widget=widgets.SliderInput(show_value=False))
     q2_communication_satisfied = models.PositiveIntegerField(min=1, max=5, widget=widgets.SliderInput(show_value=False))
     q2_communication_upset = models.PositiveIntegerField(min=1, max=5, widget=widgets.SliderInput(show_value=False))
 
-    q2_communication_fair_discussion = models.PositiveIntegerField(min=1, max=5, widget=widgets.SliderInput(show_value=False))
-    q2_communication_fear_cirticism = models.PositiveIntegerField(min=1, max=5, widget=widgets.SliderInput(show_value=False))
+    q2_communication_fair_discussion = models.PositiveIntegerField(min=1, max=5,
+                                                                   widget=widgets.SliderInput(show_value=False))
+    q2_communication_fear_cirticism = models.PositiveIntegerField(min=1, max=5,
+                                                                  widget=widgets.SliderInput(show_value=False))
     q2_communication_to_be_right = models.PositiveIntegerField(min=1, max=5, widget=widgets.SliderInput(show_value=False))
 
     q2_communication_give_opinion = models.PositiveIntegerField(min=1, max=5, widget=widgets.SliderInput(show_value=False))
-    q2_communication_others_opinions = models.PositiveIntegerField(min=1, max=5, widget=widgets.SliderInput(show_value=False))
+    q2_communication_others_opinions = models.PositiveIntegerField(min=1, max=5,
+                                                                   widget=widgets.SliderInput(show_value=False))
     q2_communication_whine = models.PositiveIntegerField(min=1, max=5, widget=widgets.SliderInput(show_value=False))
     q2_communication_inputs = models.PositiveIntegerField(min=1, max=5, widget=widgets.SliderInput(show_value=False))
 
     q2_best_player = models.CharField(
-        max_length=255,  widget=widgets.RadioSelect(),
-        choices = [])
+        max_length=255, widget=widgets.RadioSelect(),
+        choices=[])
