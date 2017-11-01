f1fe428c518ea700e1f2b7317993be7cc73af246
Alek Spitzer <alekspitzer@MacBook-Pro-de-M2015.local>
2017-11-01 08:00:13 -0600

    update

--- toilet_questionnaire_1/models.py
+++ toilet_questionnaire_1/models.py
@@ -37,20 +37,20 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
 
-    q1_first_game_judge_surprised = models.PositiveIntegerField(min=1, max=5, widget=widgets.SliderInput(show_value=False))
-    q1_first_game_judge_satisfied = models.PositiveIntegerField(min=1, max=5, widget=widgets.SliderInput(show_value=False))
+    q1_first_game_judge_surprised = models.PositiveIntegerField(min=1, max=10, widget=widgets.SliderInput(show_value=False))
+    q1_first_game_judge_satisfied = models.PositiveIntegerField(min=1, max=10, widget=widgets.SliderInput(show_value=False))
     q1_first_game_judge_upset = models.PositiveIntegerField(min=1, max=5, widget=widgets.SliderInput(show_value=False))
 
-    q1_players_helping_each_other = models.PositiveIntegerField(min=1, max=5, widget=widgets.SliderInput(show_value=False))
-    q1_team_spirit_or_cohesion = models.PositiveIntegerField(min=1, max=5, widget=widgets.SliderInput(show_value=False))
-    q1_important_presented_image_of_and_to_yourself = models.PositiveIntegerField(min=1, max=5, widget=widgets.SliderInput(show_value=False))
-    q1_important_maximum_and_fair_resources_to_everyone = models.PositiveIntegerField(min=1, max=5, widget=widgets.SliderInput(show_value=False))
-    q1_important_other_members_trust_in_you = models.PositiveIntegerField(min=1, max=5, widget=widgets.SliderInput(show_value=False))
-    q1_important_not_you_maximum_and_fair_resources_to_everyone = models.PositiveIntegerField(min=1, max=5, widget=widgets.SliderInput(show_value=False))
-    q1_trust_do_you_have_into_other_members = models.PositiveIntegerField(min=1, max=5, widget=widgets.SliderInput(show_value=False))
-    q1_how_much_do_you_like_the_other_team_members = models.PositiveIntegerField(min=1, max=5, widget=widgets.SliderInput(show_value=False))
-    q1_how_good_do_you_understand_solving = models.PositiveIntegerField(min=1, max=5, widget=widgets.SliderInput(show_value=False))
-    q1_how_good_do_the_other_group_members_understand_solving = models.PositiveIntegerField(min=1, max=5, widget=widgets.SliderInput(show_value=False))
+    q1_players_helping_each_other = models.PositiveIntegerField(min=1, max=10, widget=widgets.SliderInput(show_value=False))
+    q1_team_spirit_or_cohesion = models.PositiveIntegerField(min=1, max=10, widget=widgets.SliderInput(show_value=False))
+    q1_important_presented_image_of_and_to_yourself = models.PositiveIntegerField(min=1, max=10, widget=widgets.SliderInput(show_value=False))
+    q1_important_maximum_and_fair_resources_to_everyone = models.PositiveIntegerField(min=1, max=10, widget=widgets.SliderInput(show_value=False))
+    q1_important_other_members_trust_in_you = models.PositiveIntegerField(min=1, max=10, widget=widgets.SliderInput(show_value=False))
+    q1_important_not_you_maximum_and_fair_resources_to_everyone = models.PositiveIntegerField(min=1, max=10, widget=widgets.SliderInput(show_value=False))
+    q1_trust_do_you_have_into_other_members = models.PositiveIntegerField(min=1, max=10, widget=widgets.SliderInput(show_value=False))
+    q1_how_much_do_you_like_the_other_team_members = models.PositiveIntegerField(min=1, max=10, widget=widgets.SliderInput(show_value=False))
+    q1_how_good_do_you_understand_solving = models.PositiveIntegerField(min=1, max=10, widget=widgets.SliderInput(show_value=False))
+    q1_how_good_do_the_other_group_members_understand_solving = models.PositiveIntegerField(min=1, max=10, widget=widgets.SliderInput(show_value=False))
 
     q1_how_to_improve_the_groups_behavior_to_maximize_resources = models.TextField()
     q1_if_communicate_what_would_you_say_and_why = models.TextField()
